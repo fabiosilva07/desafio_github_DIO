d75900a637a4ff6f3fe4e5796709eb27e3fa2e83
fabiosilva07 <fabiosilva.trab@gmail.com>
2022-08-09 00:44:50 +0000

    teste 2

--- projetofinalDIO/dockerfiledb/banco.sql
+++ projetofinalDIO/dockerfiledb/banco.sql
@@ -9,4 +9,8 @@ CREATE TABLE dados (
     Host varchar(50)
 );
 
-ALTER USER 'root IDENTIFIED WITH mysql_native_password BY 'root';
+CREATE USER 'admin'@'%' IDENTIFIED BY 'admin';
+
+GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION;
+
+FLUSH PRIVILEGES;
